6f2cf562f3d3fc10ca688b686e44dadce6d0681b
ItaloCosta-dev <italocosta.dev@gmail.com>
2025-03-13 10:23:35 -0300

    feat: Tipos de dados.

---
+++ Tipos de dados/tipos_avancados_de_convercao.py
@@ -0,0 +1,10 @@
+#NoneType
+resposta = None
+print(type(resposta))
+
+#TypeCasting
+idade = 25
+idade_numero = int(idade)
+pi_inteiro = int(3.14)
+lista_para_tupla = tuple([1, 2, 3])
+print(idade, idade_numero, pi_inteiro, lista_para_tupla)
